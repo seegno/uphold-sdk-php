dbad40f7d3872328ef0e07f1d6a8164295b3a00c
Nuno Rafael Rocha <nrr@seegno.com>
2015-01-30 10:18:30 +0000

    Remove unnecessary leading slash

--- examples/Ticker.php
+++ examples/Ticker.php
@@ -2,7 +2,7 @@
 
 require_once 'vendor/autoload.php';
 
-use \Bitreserve\BitreserveClient as Client;
+use Bitreserve\BitreserveClient as Client;
 
 // Initialize the client. In this case, we don't need an
 // AUTHORIZATION_TOKEN because the Ticker endpoint is public.
