a6efa5627726c93050cf290d25b4cdd0031caea8
Nuno Rafael Rocha <nrr@seegno.com>
2015-04-06 20:25:06 +0100

    Improve functional test case docblock

--- test/Bitreserve/Tests/Functional/TestCase.php
+++ test/Bitreserve/Tests/Functional/TestCase.php
@@ -15,6 +15,9 @@ class TestCase extends \PHPUnit_Framework_TestCase
 {
     protected $client;
 
+    /**
+     * SetUp.
+     */
     public function setUp()
     {
         $client = new BitreserveClient();
