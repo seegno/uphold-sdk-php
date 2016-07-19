3896bac102d3e4ec30beac7a505ccf32b61564cc
Nuno Rafael Rocha <nunorafaelrocha@gmail.com>
2016-07-19 17:43:24 +0100

    Add command to revoke a token
    This adds `RevokeTokenCommand` that allows revoking a user's token.

--- lib/Uphold/console.php
+++ lib/Uphold/console.php
@@ -2,11 +2,13 @@
 
 require 'vendor/autoload.php';
 
-use Uphold\Command\CreateTokenCommand;
 use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Shell;
+use Uphold\Command\CreateTokenCommand;
+use Uphold\Command\RevokeTokenCommand;
 
 $application = new Application('Uphold SDK PHP');
 $application->add(new CreateTokenCommand());
+$application->add(new RevokeTokenCommand());
 
 $application->run();
